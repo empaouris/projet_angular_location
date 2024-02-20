97a853a6a7fae7b7020327296fb2bf597ab657ba
Evengyl <dark.evengyl@gmail.com>
2024-02-20 12:01:58 +0100

    add new version

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,10 @@
-import { Component, NgModule } from '@angular/core';
+import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { TransportComponent } from './share/transport/transport.component';
 import { ImmeubleComponent } from './share/immeuble/immeuble.component';
-import { VeloComponent } from './share/transport/velo/velo.component';
 import { MotoComponent } from './share/transport/moto/moto.component';
+import { TransportComponent } from './share/transport/transport.component';
+import { VeloComponent } from './share/transport/velo/velo.component';
 import { VoitureComponent } from './share/transport/voiture/VoitureComponent';
-
 
 const routes: Routes = [
   {path: 'immeuble' , component: ImmeubleComponent},
@@ -20,6 +19,4 @@ const routes: Routes = [
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule {
-  
- }
+export class AppRoutingModule { }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,16 @@
 import { NgModule } from '@angular/core';
-import { BrowserModule, provideClientHydration } from '@angular/platform-browser';
+import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ImmeubleComponent } from './share/immeuble/immeuble.component';
-import { TransportComponent } from './share/transport/transport.component';
 import { MenuComponent } from './share/menu/menu.component';
-import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
-import { VeloComponent } from './share/transport/velo/velo.component';
 import { MotoComponent } from './share/transport/moto/moto.component';
+import { TransportComponent } from './share/transport/transport.component';
+import { VeloComponent } from './share/transport/velo/velo.component';
 import { VoitureComponent } from './share/transport/voiture/VoitureComponent';
 import { CarModelComponent } from './share/transport/voiture/model/car-model/car-model.component';
-import { RouterModule } from '@angular/router';
-
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 @NgModule({
   declarations: [
@@ -30,10 +28,7 @@ import { RouterModule } from '@angular/router';
     AppRoutingModule,
     NgbModule,
   ],
-  providers: [
-    provideClientHydration()
-  ],
+  providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
-  
